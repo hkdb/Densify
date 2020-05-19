f1e5c2601ec9cc5f2274051008b3e72707653ade
hkdb <38838639+hkdb@users.noreply.github.com>
2020-05-20 02:17:05 +0800

    Added metainfo and automated versioning to AppImage building

--- AppImage/src/signAppImage.sh
+++ AppImage/src/signAppImage.sh
@@ -4,6 +4,18 @@
 raw=$(awk '/VERSION:/{getline; print}' AppDir/opt/Densify/densify)
 version=${raw:4}
 
+# Determine Today's Date
+date=$(date +"%Y-%m-%d")
+
+# Install MentaInfo
+mkdir -p ./AppDir/usr/share/metainfo
+install -D -m 644 ../src/io.3df.osi.densify.metainfo.xml ./AppDir/usr/share/metainfo/
+
+# Insert Version to MetaInfo
+vnum=${version:1}
+line="/<releases>/a\      <release version=\"$vnum\" date=\"$date\">"
+sed -i "$line" ./AppDir/usr/share/metainfo/io.3df.osi.densify.metainfo.xml
+
 # Sign AppImage
 ./appimagetool-x86_64.AppImage AppDir --sign --sign-key 205CCAC6515402D4B1F83A3622984F9A0EA52036
 
