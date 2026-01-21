af3951c41c94462ce74845c32ee4895c70e2816c
hkdb <hkdb@3df.io>
2026-01-20 21:04:33 -0800

    Added 22.04 build dep

--- AppImage/bootstrap.sh
+++ AppImage/bootstrap.sh
@@ -1,1 +1,1 @@
-apt install -y libgtk-4-1 libgtk-4-dev libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0 ghostscript
+apt install -y libgtk-4-1 libgtk-4-dev libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0 ghostscript meson ninja-build
